25548df223cfbf2d793822022df5aae64eb408c4
Kinzoman <fazavire@gmail.com>
2019-01-23 00:01:20 -0200

    tileset

---
+++ tiled/roguelikeCity_magenta.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="roguelikeCity_magenta" tilewidth="16" tileheight="16" spacing="1" tilecount="1036" columns="37">
+ <image source="../../../../../../Roguelike Modern City pack/Spritesheet/roguelikeCity_magenta.png" trans="ff00ff" width="628" height="475"/>
+</tileset>
---
+++ tiled/roguelikeCity_transparent.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="roguelikeCity_transparent" tilewidth="16" tileheight="16" tilecount="1131" columns="39">
+ <image source="../../../../../../Roguelike Modern City pack/Spritesheet/roguelikeCity_transparent.png" width="628" height="475"/>
+</tileset>
